b3f4cc8532f7226baa69f9103e29e2bb7331894f
Sanbin <andy@doipc.com>
2022-06-01 14:24:10 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace OperateCamera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Photograph = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.tscbxCameras = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.videoSourcePlayer = new AForge.Controls.VideoSourcePlayer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Photograph
+            // 
+            this.Photograph.Location = new System.Drawing.Point(532, 17);
+            this.Photograph.Name = "Photograph";
+            this.Photograph.Size = new System.Drawing.Size(75, 23);
+            this.Photograph.TabIndex = 1;
+            this.Photograph.Text = "拍照";
+            this.Photograph.UseVisualStyleBackColor = true;
+            this.Photograph.Click += new System.EventHandler(this.Photograph_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(407, 17);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "关闭";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // tscbxCameras
+            // 
+            this.tscbxCameras.FormattingEnabled = true;
+            this.tscbxCameras.Location = new System.Drawing.Point(121, 20);
+            this.tscbxCameras.Name = "tscbxCameras";
+            this.tscbxCameras.Size = new System.Drawing.Size(121, 20);
+            this.tscbxCameras.TabIndex = 7;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(289, 17);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 8;
+            this.btnConnect.Text = "连接";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "视频输入设备：";
+            // 
+            // videoSourcePlayer
+            // 
+            this.videoSourcePlayer.Location = new System.Drawing.Point(28, 64);
+            this.videoSourcePlayer.Name = "videoSourcePlayer";
+            this.videoSourcePlayer.Size = new System.Drawing.Size(640, 480);
+            this.videoSourcePlayer.TabIndex = 10;
+            this.videoSourcePlayer.Text = "videoSourcePlayer";
+            this.videoSourcePlayer.VideoSource = null;
+            this.videoSourcePlayer.MouseDown += new System.Windows.Forms.MouseEventHandler(this.videoSourcePlayer_MouseDown);
+            this.videoSourcePlayer.MouseMove += new System.Windows.Forms.MouseEventHandler(this.videoSourcePlayer_MouseMove);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(674, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 591);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "$MouseMove";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(405, 565);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "$MouseMove";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(664, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "同步捕捉";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(405, 584);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "$MouseMove";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(405, 602);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "$MouseMove";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(784, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "拍照";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(904, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "录像";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1339, 628);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.videoSourcePlayer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tscbxCameras);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.Photograph);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Open Camera";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Photograph;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ComboBox tscbxCameras;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Drawing;
+
+using AForge;
+using AForge.Controls;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Video.FFMPEG;
+using Size = System.Drawing.Size;
+
+namespace OperateCamera
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        private VideoFileWriter writer;     //写入到视频
+        private bool is_record_video = false;   //是否开始录像
+        private bool synchronization = false;   //同步显示
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                // 枚举所有视频输入设备
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                writer = new VideoFileWriter();
+
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+
+                foreach (FilterInfo device in videoDevices)
+                {
+                    tscbxCameras.Items.Add(device.Name);
+                }
+
+                tscbxCameras.SelectedIndex = 0;
+
+            }
+            catch (ApplicationException)
+            {
+                tscbxCameras.Items.Add("No local capture devices");
+                videoDevices = null;
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            CameraConn();
+           
+        }
+        //连接摄像头
+        private void CameraConn()
+        {
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[tscbxCameras.SelectedIndex].MonikerString);
+            videoSource.DesiredFrameSize = new System.Drawing.Size(640, 480);//2.2.5 无效
+            videoSource.DesiredFrameRate = 1;
+            videoSource.NewFrame += new NewFrameEventHandler(show_video);
+
+            videoSourcePlayer.VideoSource = videoSource;
+            videoSourcePlayer.Start();
+        }
+
+        //关闭摄像头
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            videoSourcePlayer.SignalToStop();
+            videoSourcePlayer.WaitForStop();
+        }
+
+        //主窗体关闭
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            btnClose_Click(null, null);
+        }
+
+        //拍照
+        private void Photograph_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for(int i=1;i<=1;i++)
+                {
+                    if (videoSourcePlayer.IsRunning)
+                    {
+                        BitmapSource bitmapSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                                        videoSourcePlayer.GetCurrentVideoFrame().GetHbitmap(),
+                                        IntPtr.Zero,
+                                         Int32Rect.Empty,
+                                        BitmapSizeOptions.FromEmptyOptions());
+                        PngBitmapEncoder pE = new PngBitmapEncoder();
+                        pE.Frames.Add(BitmapFrame.Create(bitmapSource));
+                        string picName = GetImagePath() + "\\" +  GetImageName(6) + ".jpg";
+                        if (File.Exists(picName))
+                        {
+                            File.Delete(picName);
+                        }
+                        using (Stream stream = File.Create(picName))
+                        {
+                            pE.Save(stream);
+                        }
+
+                        Bitmap bitmap = videoSourcePlayer.GetCurrentVideoFrame();    //获取到一帧图像
+                        pictureBox1.Image = Image.FromHbitmap(bitmap.GetHbitmap());
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("摄像头异常：" + ex.Message);
+            }
+        }
+
+        private string GetImagePath()
+        {
+            string personImgPath = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)
+                         + Path.DirectorySeparatorChar.ToString() + "PersonImg";
+            if (!Directory.Exists(personImgPath))
+            {
+                Directory.CreateDirectory(personImgPath);
+            }
+
+            return personImgPath;
+        }
+
+
+        private string GetImageName(int Length)
+        {
+            System.Text.StringBuilder newRandom = new System.Text.StringBuilder(62);
+            Random rd = new Random();
+            for (int i = 0; i < Length; i++)
+            {
+                newRandom.Append(constant[rd.Next(62)]);
+            }
+            return newRandom.ToString();
+        }
+
+        private static char[] constant = 
+        {
+            '0','1','2','3','4','5','6','7','8','9',
+            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+        };
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            System.Drawing.Point mouseLoction = pictureBox1.PointToClient(Control.MousePosition);
+            String mousePlace = "$MouseMove:" + mouseLoction.X.ToString() + "," + mouseLoction.Y.ToString();
+            label2.Text = mousePlace;
+        }
+
+        double x1, x2, y1, y2;
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            String MouseMsg = "$MouseEvent||";
+            System.Drawing.Point mouseLoction = pictureBox1.PointToClient(Control.MousePosition);
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    MouseMsg = "LeftMouseDown ||" + mouseLoction.X.ToString() + "||" + mouseLoction.Y.ToString() + "||";
+                    x1 = mouseLoction.X;
+                    y1 = mouseLoction.Y;
+                    break;
+                case MouseButtons.Right:
+                    MouseMsg = "RightMouseDown ||" + mouseLoction.X.ToString() + "||" + mouseLoction.Y.ToString() + "||";
+                    x2 = mouseLoction.X;
+                    y2 = mouseLoction.Y;
+                    break;
+                default:
+                    break;
+            }
+
+            //Return Math.Sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
+            label3.Text = MouseMsg + " || L:" + Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2)) + " >>> " + videoDevices[tscbxCameras.SelectedIndex].MonikerString;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //timer1.Enabled = !timer1.Enabled;
+            synchronization = !synchronization;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (videoSourcePlayer.IsRunning)
+                {
+                    BitmapSource bitmapSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                                    videoSourcePlayer.GetCurrentVideoFrame().GetHbitmap(),
+                                    IntPtr.Zero,
+                                        Int32Rect.Empty,
+                                    BitmapSizeOptions.FromEmptyOptions());
+                    PngBitmapEncoder pE = new PngBitmapEncoder();
+                    pE.Frames.Add(BitmapFrame.Create(bitmapSource));
+                    string picName = GetImagePath() + "\\" + "On-line" + ".jpg";
+                    if (File.Exists(picName))
+                    {
+                        File.Delete(picName);
+                    }
+                    using (Stream stream = File.Create(picName))
+                    {
+                        pE.Save(stream);
+                    }
+
+                    pictureBox1.ImageLocation = picName;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("摄像头异常：" + ex.Message);
+            }
+        }
+
+        private void videoSourcePlayer_MouseDown(object sender, MouseEventArgs e)
+        {
+            String MouseMsg = "$MouseEvent:";
+            System.Drawing.Point mouseLoction = videoSourcePlayer.PointToClient(Control.MousePosition);
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    MouseMsg = "LeftMouseDown :" + mouseLoction.X.ToString() + "||" + mouseLoction.Y.ToString();
+                    x1 = mouseLoction.X;
+                    y1 = mouseLoction.Y;
+                    label3.Text = MouseMsg;
+                    break;
+                case MouseButtons.Right:
+                    MouseMsg = "RightMouseDown :" + mouseLoction.X.ToString() + "||" + mouseLoction.Y.ToString();
+                    x2 = mouseLoction.X;
+                    y2 = mouseLoction.Y;
+                    label4.Text = MouseMsg;
+                    break;
+                default:
+                    break;
+            }
+
+            //Return Math.Sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
+            label5.Text = "Distance :" + Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+        }
+
+        private void videoSourcePlayer_MouseMove(object sender, MouseEventArgs e)
+        {
+            System.Drawing.Point mouseLoction = videoSourcePlayer.PointToClient(Control.MousePosition);
+            String mousePlace = "$MouseMove:" + mouseLoction.X.ToString() + "," + mouseLoction.Y.ToString();
+            label2.Text = mousePlace;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = this.videoSourcePlayer.GetCurrentVideoFrame();
+            bitmap.Save(GetImagePath() + "\\" + "img.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {            
+            if (is_record_video)
+            {
+                is_record_video = false;
+                //synchronization = false;
+
+                writer.Close();
+
+                button3.BackColor = Color.White;
+                button3.Text = "录制";
+            }
+            else {
+                int width = 640;    //录制视频的宽度
+                int height = 480;   //录制视频的高度
+                int fps = 8;
+
+                //创建一个视频文件
+                if (videoSourcePlayer.IsRunning)
+                {
+
+                    writer.Open(GetImagePath() + "\\" + GetImageName(6) + ".avi", width, height, fps, VideoCodec.MPEG4);
+
+                    is_record_video = true;
+                    //synchronization = true;
+
+                    button3.BackColor = Color.Red;
+                    button3.Text = "结束录制";
+                }
+            }
+        }
+
+        //新帧的触发函数
+        private void show_video(object sender, NewFrameEventArgs eventArgs)
+        {
+             Bitmap bitmap = eventArgs.Frame;
+
+             if (synchronization)
+             {
+                 pictureBox1.Image = Image.FromHbitmap(bitmap.GetHbitmap());
+             }
+
+            if (is_record_video)
+            {
+                writer.WriteVideoFrame(bitmap);
+                //bitmap.Dispose();
+            }
+        }
+    }
+}
